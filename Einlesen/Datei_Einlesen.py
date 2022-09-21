3d7b0cced424effdd652f78d4ea130b4e8c7c4cd
LauzeDao <96375489+LauzeDao@users.noreply.github.com>
2022-09-21 17:23:51 +0200

    Add files via upload

---
+++ Einlesen/Datei_Einlesen.py
@@ -0,0 +1,8 @@
+#Datei einlesen
+# https://www.python-lernen.de/dateien-auslesen.htm
+# w für write, a für append, r für read, r+ für lesen und schreiben, b in Binär lesen und schreiben
+
+fobj = open('text.txt', 'r')
+print(fobj.read())
+
+fobj.close()
