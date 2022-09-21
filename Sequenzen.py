006a1e08a61dafd5c1de4e26d024bb834ecb538a
LauzeDao <96375489+LauzeDao@users.noreply.github.com>
2022-09-21 16:05:50 +0200

    Add files via upload

---
+++ Sequenzen.py
@@ -0,0 +1,22 @@
+# Sequenzen
+
+t = (1, 45, 'Tom', 3.14, 'Martyna', 'Lauze', 12387493, 3.45453)
+
+print(t[2])
+
+x = len(t) -1
+
+u = ( t[2:x])
+print(u)
+
+t3 = t + t + t
+
+tupel3 = t * 3
+
+d = (t3)
+print(d[::-1])
+
+e =(t3)
+print(e[::2])
+
+print( 'Tom' in t3)
