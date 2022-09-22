91ad305291472a641c5ace5877a8e4c53c8fcd36
LauzeDao <96375489+LauzeDao@users.noreply.github.com>
2022-09-22 13:48:10 +0200

    update link

--- Einlesen/Datei_Einlesen.py
+++ Einlesen/Datei_Einlesen.py
@@ -1,5 +1,6 @@
 #Datei einlesen
 # https://www.python-lernen.de/dateien-auslesen.htm
+
 # w für write, a für append, r für read, r+ für lesen und schreiben, b in Binär lesen und schreiben
 
 fobj = open('text.txt', 'r')
